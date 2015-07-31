f00f851fc73b3781f4f0a6afa507ae7de520c631
Pongsakorn Ruedeerat <devilhand999@gmail.com>
2015-07-31 21:02:59 +0700

    remove hamberger button in "Completed Achievement" and change hint name

--- app/src/main/java/com/example/androidbp/activity/CompleteAchievement.java
+++ app/src/main/java/com/example/androidbp/activity/CompleteAchievement.java
@@ -85,9 +85,9 @@ public class CompleteAchievement extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
