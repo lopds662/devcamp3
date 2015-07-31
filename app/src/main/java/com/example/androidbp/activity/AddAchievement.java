fec3822c5671a492ddfdc006e42054e57a00263f
Pongsakorn Ruedeerat <devilhand999@gmail.com>
2015-08-01 03:07:53 +0700

    add image uploader but not sure that success

--- app/src/main/java/com/example/androidbp/activity/AddAchievement.java
+++ app/src/main/java/com/example/androidbp/activity/AddAchievement.java
@@ -21,6 +21,7 @@ import com.example.androidbp.R;
 import com.example.androidbp.api.Api;
 import com.example.androidbp.api.model.AchievementItem;
 import com.example.androidbp.api.model.CreateAchievementBody;
+import com.example.androidbp.api.model.ImageUploadResult;
 import com.example.androidbp.manager.HttpManager;
 
 import java.io.File;
@@ -28,6 +29,7 @@ import java.io.File;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+import retrofit.mime.TypedFile;
 
 public class AddAchievement extends ActionBarActivity {
 
@@ -182,12 +184,27 @@ public class AddAchievement extends ActionBarActivity {
         }
     }
 
-    public void addAchievement(String achievement){
+    public void addAchievement(final String achievement){
+        TypedFile typedImage = new TypedFile("application/octet-stream", file);
+        HttpManager.ApiFor(Api.class).uploadImage(typedImage, new Callback<ImageUploadResult>() {
+            @Override
+            public void success(ImageUploadResult imageUploadResult, Response response) {
+                callHttpCreateAchievement(imageUploadResult.id, achievement);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e("GGG", error.getMessage());
+            }
+        });
+    }
+
+    private void callHttpCreateAchievement(String imageId, String achievement) {
         CreateAchievementBody body = new CreateAchievementBody();
         body.title = achievement;
         body.latitude = 50.00f;
         body.longitude = 40.00f;
-        body.image_id = "asdfasdfsadf";
+        body.image_id = imageId;
         HttpManager.ApiFor(Api.class).createNewAchievement(body, new Callback<AchievementItem>() {
             @Override
             public void success(AchievementItem achievementItem, Response response) {
--- app/src/main/java/com/example/androidbp/api/Api.java
+++ app/src/main/java/com/example/androidbp/api/Api.java
@@ -3,6 +3,7 @@ package com.example.androidbp.api;
 import com.example.androidbp.api.model.AchievementItem;
 import com.example.androidbp.api.model.ArchivementFeedItem;
 import com.example.androidbp.api.model.CreateAchievementBody;
+import com.example.androidbp.api.model.ImageUploadResult;
 import com.example.androidbp.api.model.Repo;
 
 import java.util.List;
@@ -10,8 +11,11 @@ import java.util.List;
 import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.GET;
+import retrofit.http.Multipart;
 import retrofit.http.POST;
+import retrofit.http.Part;
 import retrofit.http.Path;
+import retrofit.mime.TypedFile;
 
 /**
  * Created by sharp on 7/29/2015 AD.
@@ -25,4 +29,8 @@ public interface Api {
 
     @POST("/post/add_post")
     void createNewAchievement(@Body CreateAchievementBody body, Callback<AchievementItem> cb);
+
+    @POST("/image/upload")
+    @Multipart
+    void uploadImage(@Part("file") TypedFile file, Callback<ImageUploadResult> cb);
 }
